82bd86f6af5abc563d1cf5b1fbf79ca033a4d3d4
milena-t <milena.trabert@t-online.de>
2025-09-30 11:21:07 +0200

    add path

--- bash/remeatmasker_Tfre.sh
+++ bash/remeatmasker_Tfre.sh
@@ -2,4 +2,4 @@
 
 REPLIB=/proj/naiss2023-6-65/Milena/chapter3/repeat_libraries/Tfre
 ASSEMBLY=/proj/naiss2023-6-65/Milena/chapter3/species_assemblies/Tfre_GCA_022388455.1.fasta 
-sbatch run_repeatmasker.sh  $ASSEMBLY $REPLIB Tfre
+sbatch /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/run_repeatmasker.sh  $ASSEMBLY $REPLIB Tfre
