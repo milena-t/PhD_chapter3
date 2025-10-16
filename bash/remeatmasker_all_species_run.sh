3ca26a55736ba670ee7f710b35fbefb11a1162d3
milena-t <milena.trabert@t-online.de>
2025-10-16 11:33:05 +0200

    remask Tfre

--- bash/remeatmasker_all_species_run.sh
+++ bash/remeatmasker_all_species_run.sh
@@ -2,8 +2,8 @@
 
 REPLIB=/proj/naiss2023-6-65/Milena/chapter3/repeat_libraries/Tfre
 ASSEMBLY=/proj/naiss2023-6-65/Milena/chapter3/species_assemblies/Tfre_GCA_022388455.1.fasta 
-# sbatch /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/run_repeatmasker.sh  $ASSEMBLY $REPLIB Tfre
+sbatch /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/run_repeatmasker.sh  $ASSEMBLY $REPLIB Tfre
 
 ASSEMBLY=/proj/naiss2023-6-65/Milena/chapter3/species_assemblies/Cmag_GCA_965644565.1.fasta
 REPLIB=/proj/naiss2023-6-65/Milena/chapter3/repeat_libraries/Cmag
-sbatch /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/run_repeatmasker.sh  $ASSEMBLY $REPLIB Cmag
+# sbatch /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/run_repeatmasker.sh  $ASSEMBLY $REPLIB Cmag
