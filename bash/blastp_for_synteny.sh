26a1feb3095e517ca1c37e697ed884bd6ab371f3
milena-t <milena.trabert@t-online.de>
2025-10-27 10:56:15 +0100

    add c_septempunctata

--- bash/blastp_for_synteny.sh
+++ bash/blastp_for_synteny.sh
@@ -16,6 +16,7 @@ module load bioinfo-tools blast/2.15.0+
 # sed 's/>C_chinensis__C_chinensis__C_chinensis_/>/g' /proj/naiss2023-6-65/Milena/chapter2/protein_data/C_chinensis.faa > /proj/naiss2023-6-65/Milena/chapter2/protein_data/C_chinensis_original_header.faa
 # sed 's/>Cmac_Lome_diverse_/>/g' /proj/naiss2023-6-65/Milena/chapter2/protein_data/C_maculatus_superscaffolded.faa > /proj/naiss2023-6-65/Milena/chapter2/protein_data/C_maculatus_superscaffolded_original_header.faa
 # sed 's/>T_castaneum__T_castaneum__T_castaneum_/>/g' /proj/naiss2023-6-65/Milena/chapter2/protein_data/T_castaneum.faa > /proj/naiss2023-6-65/Milena/chapter2/protein_data/T_castaneum_original_header.faa
+sed 's/>C_septempunctata__C_septempunctata__C_septempunctata_/>/g' /proj/naiss2023-6-65/Milena/chapter2/protein_data/C_septempunctata.faa > /proj/naiss2023-6-65/Milena/chapter3/protein_data/C_septempunctata_original_header.faa
 
 A_obtectus_proteins=/proj/naiss2023-6-65/Milena/chapter3/protein_data/A_obtectus_original_header.faa
 B_siliquastri_proteins=/proj/naiss2023-6-65/Milena/chapter3/protein_data/B_siliquastri_original_header.faa
@@ -24,29 +25,30 @@ C_maculatus_proteins=/proj/naiss2023-6-65/Milena/chapter3/protein_data/C_maculat
 T_castaneum_proteins=/proj/naiss2023-6-65/Milena/chapter3/protein_data/T_castaneum_original_header.faa
 T_freemani_proteins=/proj/naiss2023-6-65/Milena/chapter3/protein_data/T_freemani_original_header.faa
 C_magnifica_proteins=/proj/naiss2023-6-65/Milena/chapter3/protein_data/C_magnifica_original_header.faa
+C_septempunctata_proteins=/proj/naiss2023-6-65/Milena/chapter3/protein_data/C_septempunctata_original_header.faa
 
 OUTDIR=/proj/naiss2023-6-65/Milena/chapter3/all_vs_all_blastp
 
-# ## --> re-run for new proteinfiles!
-# 
-# ## make databases
-# for SPECIES1 in $T_freemani_proteins $C_magnifica_proteins # $A_obtectus_proteins $B_siliquastri_proteins $C_chinensis_proteins $C_maculatus_proteins $T_castaneum_proteins
-# do
-#     makeblastdb -in $SPECIES1 -dbtype prot
-#     echo " ---> done database ${SPECIES1}"
-# done
-# 
-# ## -->
+## --> re-run for new proteinfiles!
+
+## make databases
+for SPECIES1 in $C_septempunctata_proteins # $T_freemani_proteins $C_magnifica_proteins # $A_obtectus_proteins $B_siliquastri_proteins $C_chinensis_proteins $C_maculatus_proteins $T_castaneum_proteins
+do
+    makeblastdb -in $SPECIES1 -dbtype prot
+    echo " ---> done database ${SPECIES1}"
+done
+
+## -->
 
 
 
-for SPECIES1 in $A_obtectus_proteins $B_siliquastri_proteins $C_chinensis_proteins $C_maculatus_proteins $T_castaneum_proteins $T_freemani_proteins $C_magnifica_proteins
+for SPECIES1 in $C_septempunctata_proteins # $A_obtectus_proteins $B_siliquastri_proteins $C_chinensis_proteins $C_maculatus_proteins $T_castaneum_proteins $T_freemani_proteins $C_magnifica_proteins
 do  
 
     SPECIES1_name="${SPECIES1##*/}"
     SPECIES1_name="${SPECIES1_name%.*}"
 
-    for SPECIES2 in $A_obtectus_proteins $B_siliquastri_proteins $C_chinensis_proteins $C_maculatus_proteins $T_castaneum_proteins $T_freemani_proteins $C_magnifica_proteins
+    for SPECIES2 in $A_obtectus_proteins $B_siliquastri_proteins $C_chinensis_proteins $C_maculatus_proteins $T_castaneum_proteins $T_freemani_proteins $C_magnifica_proteins # $C_septempunctata_proteins
     do
 
         SPECIES2_name="${SPECIES2##*/}"
@@ -65,9 +67,10 @@ do
         blastp -query $SPECIES1 -db $SPECIES2 -out $OUT_1v2 -num_threads 5 -num_alignments 5 -evalue 1e-10  -outfmt 6
         echo " =========> ${OUT_1v2} done!"
 
-        # # reverse already happens automatically in the nested for loop no need to implement explicitly
-        # blastp -query $SPECIES2 -db $SPECIES1 -out $OUT_2v1 -num_threads 5 -num_alignments 5 -evalue 1e-10  -outfmt 6
-        # echo " =========> ${OUT_2v1} done!"
+        # reverse already happens automatically in the nested for loop no need to implement explicitly
+        echo "RUN: blastp -query $SPECIES2 -db $SPECIES1 -out $OUT_2v1 -num_threads 5 -num_alignments 5 -evalue 1e-10  -outfmt 6"
+        blastp -query $SPECIES2 -db $SPECIES1 -out $OUT_2v1 -num_threads 5 -num_alignments 5 -evalue 1e-10  -outfmt 6
+        echo " =========> ${OUT_2v1} done!"
 
     done
 done
