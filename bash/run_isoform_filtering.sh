d32013bf00df58f3d2e3d799d8bcc65583da112e
milena-t <milena.trabert@t-online.de>
2025-11-17 17:55:47 +0100

    fix path

--- bash/run_isoform_filtering.sh
+++ bash/run_isoform_filtering.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
 cd /proj/naiss2023-6-65/Milena/coleoptera_sequences/d_carinulata/
-sbatch /Users/miltr339/work/PhD_code/PhD_chapter3/bash/isoform_filter_gff.sh GCF_026250575.1_icDioCari1.1_genomic.gff
+sbatch /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/isoform_filter_gff.sh GCF_026250575.1_icDioCari1.1_genomic.gff
 
 cd /proj/naiss2023-6-65/Milena/coleoptera_sequences/d_carinulata/
-sbatch /Users/miltr339/work/PhD_code/PhD_chapter3/bash/isoform_filter_gff.sh GCF_026250575.1_icDioCari1.1_genomic.gff
+sbatch /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/isoform_filter_gff.sh GCF_026250575.1_icDioCari1.1_genomic.gff
