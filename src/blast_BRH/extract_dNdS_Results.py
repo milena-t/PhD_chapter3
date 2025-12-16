fdc80a10c35cd1f90f9a02d0b7776d0dd3bef699
milena-t <milena.trabert@t-online.de>
2025-12-16 17:54:38 +0100

    start eval

---
+++ src/blast_BRH/extract_dNdS_Results.py
@@ -0,0 +1,7 @@
+"""
+extract the dNdS values from the results
+example filepath:
+A_obtectus_B_siliquastri_pairwise_dNdS/A_obtectus_B_siliquastri_A-linked_ortholog_1122_dNdS/2NG.dNdS
+make violinplot or boxplot or something
+"""
+
