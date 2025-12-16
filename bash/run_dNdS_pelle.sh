d87f3c9af4a9d1d0c01fa08e4ba1f145d2959065
milena-t <milena.trabert@t-online.de>
2025-12-16 14:20:13 +0100

    add pelle test

---
+++ bash/run_dNdS_pelle.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#!/bin/bash -l
+#SBATCH -A uppmax2025-2-148
+#SBATCH -c 1
+#SBATCH --mem=6G
+#SBATCH -t 1-00:00:00
+#SBATCH --mail-type=ALL
+#SBATCH -J mummer_alignment
+#SBATCH -o mummer_alignment.log
+
+ml FastTree/2.2-GCCcore-13.3.0 Biopython/1.84-foss-2024a
+
+PAL2NAL_UPPMAX=/proj/naiss2023-6-65/Lila/beetle_genomes/pal2nal.v14/pal2nal.pl
+CLUSTALO_UPPMAX=/proj/naiss2023-6-65/Milena/software_install/clustal_omega/clustal-omega-1.2.4/bin/clustalo
+PAML_UPPMAX=/sw/bioinfo/paml/4.10.7/rackham/bin/codeml
+IN_DIR=/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_X/
+
+python3 /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/src/blast_BRH/calculate_pairwise_dNdS.py \
+    --cds "${IN_DIR}B_siliquastri_C_maculatus_X-linked_ortholog_166.fasta" \
+    --pal2nalbin $PAL2NAL_UPPMAX \
+    --codeml \
+    --codemlbin $PAML_UPPMAX \
+    --clustalbin $CLUSTALO_UPPMAX \
+    --verbose \
+    
+    # --overwrite
+    # --fasttreebin /Users/miltr339/Desktop/FastTree \
+    # --yn00 \
+    # --yn00bin /Users/miltr339/work/paml/src/yn00 \
+
