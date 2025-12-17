bc00e6e66ba28794cd452460cc8bf85bb9e79f94
milena-t <milena.trabert@t-online.de>
2025-12-17 16:49:59 +0100

    fix path

--- bash/run_extract_dNdS_results.sh
+++ bash/run_extract_dNdS_results.sh
@@ -2,13 +2,13 @@
 #SBATCH -A uppmax2025-2-148
 #SBATCH -c 1
 #SBATCH --mem=20G
-#SBATCH -t 1-00:00:00
+#SBATCH -t 10:00:00
 #SBATCH --mail-type=ALL
 #SBATCH -J run_extract_dNdS_results
 #SBATCH -o run_extract_dNdS_results.log
 
 module load Biopython/1.84-foss-2024a
 
-echo "python3 ../src/blast_BRH/extract_dNdS_results.py" 
+echo "python3 /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/src/blast_BRH/extract_dNdS_results.py" 
 echo 
-python3 ../src/blast_BRH/extract_dNdS_results.py
+python3 /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/src/blast_BRH/extract_dNdS_results.py
