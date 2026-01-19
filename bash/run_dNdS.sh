118dd1675f8350248ff81559dab9e43086562f35
milena-t <milena.trabert@t-online.de>
2026-01-19 15:33:34 +0100

    testing

--- bash/run_dNdS.sh
+++ bash/run_dNdS.sh
@@ -6,7 +6,7 @@ CLUSTALO_UPPMAX=/proj/naiss2023-6-65/Milena/software_install/clustal_omega/clust
 PAML_UPPMAX=/sw/bioinfo/paml/4.10.7/rackham/bin/codeml
 
 python3 /Users/miltr339/work/PhD_code/PhD_chapter3/src/blast_BRH/calculate_pairwise_dNdS.py \
-    --cds /Users/miltr339/work/pairwise_blast_chapter_2_3/brh_tables/brh_sequences_X/B_siliquastri_C_maculatus_X-linked_ortholog_166.fasta \
+    --cds /Users/miltr339/work/pairwise_blast_chapter_2_3/brh_tables/brh_sequences_X/B_siliquastri_C_maculatus_X-linked_ortholog_164.fasta \
     --pal2nalbin /Users/miltr339/work/pal2nal.v14/pal2nal.pl \
     --codeml \
     --codemlbin /Users/miltr339/work/paml/src/codeml \
