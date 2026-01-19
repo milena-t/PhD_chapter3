04dddc20833b6f3599aa4e0f9a8effad0e6b138d
milena-t <milena.trabert@t-online.de>
2026-01-19 18:03:50 +0100

    ortholog_Fastas

---
+++ bash/upload_ortholog_fastas.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+
+rsync -azP A_obtectus_B_siliquastri_A-linked_ortholog_* milenatr@pelle.uppmax.uu.se:/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_A/new_close_comparisons/
+rsync -azP A_obtectus_C_chinensis_A-linked_ortholog_* milenatr@pelle.uppmax.uu.se:/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_A/new_close_comparisons/
+rsync -azP A_obtectus_C_maculatus_A-linked_ortholog_* milenatr@pelle.uppmax.uu.se:/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_A/new_close_comparisons/
+
+rsync -azP B_siliquastri_C_chinensis_A-linked_ortholog_* milenatr@pelle.uppmax.uu.se:/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_A/new_close_comparisons/
+rsync -azP B_siliquastri_C_maculatus_A-linked_ortholog_* milenatr@pelle.uppmax.uu.se:/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_A/new_close_comparisons/
+
+rsync -azP C_chinensis_C_maculatus_A-linked_ortholog_* milenatr@pelle.uppmax.uu.se:/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_A/new_close_comparisons/
+
+rsync -azP D_carinulata_D_sublineata_A-linked_ortholog_* milenatr@pelle.uppmax.uu.se:/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_A/new_close_comparisons/
+
