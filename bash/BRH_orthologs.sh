f73ceda5220a50fb7d2bfaa001f948a3edf72ff9
milena-t <milena.trabert@t-online.de>
2026-01-19 18:43:06 +0100

    new species

--- bash/BRH_orthologs.sh
+++ bash/BRH_orthologs.sh
@@ -6,9 +6,6 @@ ANN_DIR=/Users/miltr339/work/chapter3/isoform_filtered_native_annotations
 SCRIPT_PATH=/Users/miltr339/work/PhD_code/PhD_chapter3/src/blast_BRH
 
 
-python3 $SCRIPT_DIR/get_blast_BRH.py $BLAST_OUTDIR/T_castaneum_original_header_vs_T_freemani_original_header.blast $BLAST_OUTDIR/T_freemani_original_header_vs_T_castaneum_original_header.blast
-python3 $SCRIPT_DIR/get_blast_BRH.py $BLAST_OUTDIR/C_magnifica_original_header_vs_C_septempunctata_original_header.blast $BLAST_OUTDIR/C_septempunctata_original_header_vs_C_magnifica_original_header.blast
-
 python3 $SCRIPT_PATH/get_blast_BRH.py \
 --blast1 $BLAST_OUTDIR/T_castaneum_original_header_vs_T_freemani_original_header.blast \
 --blast2 $BLAST_OUTDIR/T_freemani_original_header_vs_T_castaneum_original_header.blast \
@@ -16,16 +13,16 @@ python3 $SCRIPT_PATH/get_blast_BRH.py \
 --annotation2 $ANN_DIR/T_freemani.gff \
 --X_contigs1 NC_087403.1 \
 --X_contigs2 CM039461.1 \
---Y_contigs1 none
+--Y_contigs1 none \
 --Y_contigs2 none
 
 
-# python3 $SCRIPT_PATH/get_blast_BRH.py \
-# --blast1 $BLAST_OUTDIR/C_magnifica_original_header_vs_C_septempunctata_original_header.blast \
-# --blast2 $BLAST_OUTDIR/C_septempunctata_original_header_vs_C_magnifica_original_header.blast \
-# --annotation1 $ANN_DIR/C_magnifica.gff \
-# --annotation2 $ANN_DIR/C_septempunctata.gff \
-# --X_contigs1 OZ286750.1 \
-# --X_contigs2 NC_058198.1 \
-# --Y_contigs1 none
-# --Y_contigs2 none
+python3 $SCRIPT_PATH/get_blast_BRH.py \
+--blast1 $BLAST_OUTDIR/C_magnifica_original_header_vs_C_septempunctata_original_header.blast \
+--blast2 $BLAST_OUTDIR/C_septempunctata_original_header_vs_C_magnifica_original_header.blast \
+--annotation1 $ANN_DIR/C_magnifica.gff \
+--annotation2 $ANN_DIR/C_septempunctata.gff \
+--X_contigs1 OZ286750.1 \
+--X_contigs2 NC_058198.1 \
+--Y_contigs1 none
+--Y_contigs2 none
