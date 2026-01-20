322f52a009407c85d9114824ac32d93c629de919
milena-t <milena.trabert@t-online.de>
2026-01-20 11:29:55 +0100

    upload all

--- bash/upload_ortholog_fastas.sh
+++ bash/upload_ortholog_fastas.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -l
 
-rsync -azP A_obtectus_B_siliquastri_A-linked_ortholog_* milenatr@pelle.uppmax.uu.se:/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_A/new_close_comparisons/
-rsync -azP A_obtectus_C_chinensis_A-linked_ortholog_* milenatr@pelle.uppmax.uu.se:/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_A/new_close_comparisons/
-rsync -azP A_obtectus_C_maculatus_A-linked_ortholog_* milenatr@pelle.uppmax.uu.se:/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_A/new_close_comparisons/
 
-rsync -azP B_siliquastri_C_chinensis_A-linked_ortholog_* milenatr@pelle.uppmax.uu.se:/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_A/new_close_comparisons/
-rsync -azP B_siliquastri_C_maculatus_A-linked_ortholog_* milenatr@pelle.uppmax.uu.se:/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_A/new_close_comparisons/
+rsync -azP /Users/miltr339/work/pairwise_blast_chapter_2_3/brh_tables/brh_sequences_A milenatr@pelle.uppmax.uu.se:/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/
+rsync -azP /Users/miltr339/work/pairwise_blast_chapter_2_3/brh_tables/brh_sequences_X milenatr@pelle.uppmax.uu.se:/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/
+# rsync -azP A_obtectus_B_siliquastri_A-linked_ortholog_* milenatr@pelle.uppmax.uu.se:/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_A
+# rsync -azP A_obtectus_C_chinensis_A-linked_ortholog_* milenatr@pelle.uppmax.uu.se:/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_A
+# rsync -azP A_obtectus_C_maculatus_A-linked_ortholog_* milenatr@pelle.uppmax.uu.se:/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_A
 
-rsync -azP C_chinensis_C_maculatus_A-linked_ortholog_* milenatr@pelle.uppmax.uu.se:/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_A/new_close_comparisons/
+# rsync -azP B_siliquastri_C_chinensis_A-linked_ortholog_* milenatr@pelle.uppmax.uu.se:/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_A
+# rsync -azP B_siliquastri_C_maculatus_A-linked_ortholog_* milenatr@pelle.uppmax.uu.se:/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_A
 
-rsync -azP D_carinulata_D_sublineata_A-linked_ortholog_* milenatr@pelle.uppmax.uu.se:/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_A/new_close_comparisons/
+# rsync -azP C_chinensis_C_maculatus_A-linked_ortholog_* milenatr@pelle.uppmax.uu.se:/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_A
+
+# rsync -azP D_carinulata_D_sublineata_A-linked_ortholog_* milenatr@pelle.uppmax.uu.se:/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_A
 
