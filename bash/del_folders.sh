d3fd285cf1e1f888e34026c5c03754b788ecb0fd
milena-t <milena.trabert@t-online.de>
2026-01-20 11:16:27 +0100

    project ID

--- bash/del_folders.sh
+++ bash/del_folders.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
-#SBATCH -A uppmax2025-2-148
+#SBATCH -A uppmax2026-1-8
 #SBATCH -c 1
 #SBATCH --mem=6G
 #SBATCH -t 1-00:00:00
 #SBATCH --mail-type=ALL
-#SBATCH -J run_dNdS
-#SBATCH -o run_dNdS.log
+#SBATCH -J del_old_Fasta
+#SBATCH -o del_old_Fasta.log
 
 
 rm -r /proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_A
