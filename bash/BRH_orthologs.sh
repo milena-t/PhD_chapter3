476641b1f481e42257b4859fc5d48721e69d2634
milena-t <milena.trabert@t-online.de>
2026-01-20 13:51:25 +0100

    fix full contig names

--- bash/BRH_orthologs.sh
+++ bash/BRH_orthologs.sh
@@ -12,7 +12,7 @@ python3 $SCRIPT_PATH/get_blast_BRH.py \
 --annotation1 $ANN_DIR/T_castaneum.gff \
 --annotation2 $ANN_DIR/T_freemani.gff \
 --X_contigs1 NC_087403.1 \
---X_contigs2 CM039461.1 \
+--X_contigs2 ENA_CM039461_CM039461.1_Tribolium_freemani_isolate_YK \
 # --Y_contigs1 none \
 # --Y_contigs2 none
 
@@ -22,7 +22,7 @@ python3 $SCRIPT_PATH/get_blast_BRH.py \
 --blast2 $BLAST_OUTDIR/C_septempunctata_original_header_vs_C_magnifica_original_header.blast \
 --annotation1 $ANN_DIR/C_magnifica.gff \
 --annotation2 $ANN_DIR/C_septempunctata.gff \
---X_contigs1 OZ286750.1 \
+--X_contigs1 OZ286750.1_Coccinella_magnifica_genome_assembly,_chromosome:_X \
 --X_contigs2 NC_058198.1 \
 # --Y_contigs1 none \
 # --Y_contigs2 none
