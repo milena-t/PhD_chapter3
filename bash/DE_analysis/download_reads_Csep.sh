0c9cf8048aaa987a8e6cbc1027ddd51503812524
milena-t <milena.trabert@t-online.de>
2026-01-28 16:10:24 +0100

    install sratools

--- bash/DE_analysis/download_reads_Csep.sh
+++ bash/DE_analysis/download_reads_Csep.sh
@@ -6,18 +6,17 @@
 #SBATCH -o download_sequences.log
 #SBATCH --mail-type=ALL
 
-module load bioinfo-tools
-module load sratools/3.0.7
-module load pigz
+module load pigz/2.8-GCCcore-13.3.0
 
 # Define directories
 BASE=/proj/naiss2023-6-65/Milena/chapter3/RNAseq/Coccinella
 SRA=$BASE/raw_data/sra
 FASTQ=$BASE/raw_data/fastq
+SRAPATH=/proj/naiss2023-6-65/Milena/software_install/sra_tools/sratoolkit.3.3.0-ubuntu64/bin/
 
 mkdir -p $SRA $FASTQ
 
-prefetch $1 --output-directory $SRA # Download .sra file
+${SRAPATH}prefetch $1 --output-directory $SRA # Download .sra file
 fasterq-dump $SRA/$1/$1.sra -O $FASTQ --split-files --threads 8 # Convert to FASTQ (paired-end, gzipped)
 
 gunzip "${FASTQ}"/*
