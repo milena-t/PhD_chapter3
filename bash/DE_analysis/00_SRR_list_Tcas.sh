e15c192b25ef7aaee2c82870e97b46d5b9141005
milena-t <milena.trabert@t-online.de>
2026-01-28 16:44:08 +0100

    make Tcas list download

---
+++ bash/DE_analysis/00_SRR_list_Tcas.sh
@@ -0,0 +1,19 @@
+sbatch -J Tcas_SRR1647922_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Tfre.sh SRR1647922
+sbatch -J Tcas_SRR1647927_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Tfre.sh SRR1647927
+sbatch -J Tcas_SRR1647933_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Tfre.sh SRR1647933
+sbatch -J Tcas_SRR1647925_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Tfre.sh SRR1647925
+sbatch -J Tcas_SRR1647926_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Tfre.sh SRR1647926
+sbatch -J Tcas_SRR1647936_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Tfre.sh SRR1647936
+
+# SRR1647922  female antenna
+# SRR1647927  female body
+# SRR1647933  female head
+# SRR1647925  male antenna
+# SRR1647926  male body
+# SRR1647936  male head
+
+## Tfre SRRs
+# SRR14070854 # male adult
+# SRR14070855 # male adult
+# SRR14070871 # female adult
+# SRR14070870 # female adult
--- bash/DE_analysis/00_SRR_list_Tfre.sh
+++
@@ -1,9 +0,0 @@
-sbatch -J Tfre_SRR14070854_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Tfre.sh SRR14070854
-sbatch -J Tfre_SRR14070855_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Tfre.sh SRR14070855
-sbatch -J Tfre_SRR14070871_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Tfre.sh SRR14070871
-sbatch -J Tfre_SRR14070870_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Tfre.sh SRR14070870
-
-# SRR14070854 # male adult
-# SRR14070855 # male adult
-# SRR14070871 # female adult
-# SRR14070870 # female adult
