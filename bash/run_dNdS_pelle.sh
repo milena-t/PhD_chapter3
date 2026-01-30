fd9001feb8395cbf8d4d3c7879425527357ec9c7
milena-t <milena.trabert@t-online.de>
2026-01-30 09:54:15 +0100

    fix exec paml

--- bash/run_dNdS_pelle.sh
+++ bash/run_dNdS_pelle.sh
@@ -7,13 +7,13 @@
 #SBATCH -J run_dNdS
 #SBATCH -o run_dNdS.log
 
-ml FastTree/2.2-GCCcore-13.3.0 Biopython/1.84-foss-2024a
-
+ml FastTree/2.2-GCCcore-13.3.0 Biopython/1.84-foss-2024a PAML/4.10.9-GCCcore-13.3.0
 INFILE=$1
 
 PAL2NAL_UPPMAX=/proj/naiss2023-6-65/Lila/beetle_genomes/pal2nal.v14/pal2nal.pl
 CLUSTALO_UPPMAX=/proj/naiss2023-6-65/Milena/software_install/clustal_omega/clustal-omega-1.2.4/bin/clustalo
 PAML_UPPMAX=/sw/bioinfo/paml/4.10.7/rackham/bin/codeml
+PAML_CONFIG=/sw/bioinfo/paml/4.10.7/rackham/examples/codeml.ctl
 IN_DIR=/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_X/
 
 python3 /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/src/blast_BRH/calculate_pairwise_dNdS.py \
@@ -21,9 +21,10 @@ python3 /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/src/blast_BRH/calculat
     --pal2nalbin $PAL2NAL_UPPMAX \
     --codeml \
     --branch_model \
-    --codemlbin $PAML_UPPMAX \
+    --config_path $PAML_CONFIG \
     --clustalbin $CLUSTALO_UPPMAX \
     
+    # --codemlbin $PAML_UPPMAX \
     # --verbose \
     # --overwrite
     # --fasttreebin /Users/miltr339/Desktop/FastTree \
