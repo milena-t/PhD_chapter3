062351e0372d78b245d2f29f691e0c00583d4852
milena-t <milena.trabert@t-online.de>
2026-02-06 13:55:23 +0100

    download cmac reads

---
+++ bash/DE_analysis/00_SRR_list_Cmac.sh
@@ -0,0 +1,36 @@
+sbatch -J Cmac_SRR24182302_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Cmac.sh SRR24182302
+
+sbatch -J Cmac_SRR3113345_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Cmac.sh SRR3113345 
+sbatch -J Cmac_SRR3113346_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Cmac.sh SRR3113346
+sbatch -J Cmac_SRR3113354_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Cmac.sh SRR3113354
+sbatch -J Cmac_SRR3113353_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Cmac.sh SRR3113353
+sbatch -J Cmac_SRR3113350_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Cmac.sh SRR3113350
+sbatch -J Cmac_SRR3113352_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Cmac.sh SRR3113352
+sbatch -J Cmac_SRR3113357_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Cmac.sh SRR3113357
+sbatch -J Cmac_SRR3113356_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Cmac.sh SRR3113356
+sbatch -J Cmac_SRR3113361_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Cmac.sh SRR3113361
+sbatch -J Cmac_SRR3113362_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Cmac.sh SRR3113362
+sbatch -J Cmac_SRR3113340_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Cmac.sh SRR3113340
+sbatch -J Cmac_SRR3113341_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Cmac.sh SRR3113341
+sbatch -J Cmac_SRR3113365_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Cmac.sh SRR3113365
+sbatch -J Cmac_SRR3113366_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Cmac.sh SRR3113366
+sbatch -J Cmac_SRR3113378_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Cmac.sh SRR3113378
+sbatch -J Cmac_SRR3113342_download /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/bash/DE_analysis/download_reads_Cmac.sh SRR3113342
+
+
+# SRR3113345,Head+thorax,virgin,female
+# SRR3113346,Head+thorax,virgin,female
+# SRR3113354,Head+thorax,mated,female
+# SRR3113353,Head+thorax,mated,female
+# SRR3113350,abdomen,virgin,female
+# SRR3113352,abdomen,virgin,female
+# SRR3113357,abdomen,mated,female
+# SRR3113356,abdomen,mated,female
+# SRR3113361,Head+thorax,virgin,male
+# SRR3113362,Head+thorax,virgin,male
+# SRR3113340,Head+thorax,mated,male
+# SRR3113341,Head+thorax,mated,male
+# SRR3113365,abdomen,virgin,male
+# SRR3113366,abdomen,virgin,male
+# SRR3113378,abdomen,mated,male
+# SRR3113342,abdomen,mated,male
---
+++ bash/DE_analysis/download_reads_Cmac.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2026-1-8
+#SBATCH -c 8
+#SBATCH -t 1:00:00
+#SBATCH -J download_sequences
+#SBATCH -o download_sequences_Cmac.log
+#SBATCH --mail-type=ALL
+
+module load pigz/2.8-GCCcore-13.3.0
+
+# Define directories
+BASE=/proj/naiss2023-6-65/Milena/chapter3/RNAseq/maculatus
+SRA=$BASE/raw_data/sra
+FASTQ=$BASE/raw_data/fastq
+SRAPATH=/proj/naiss2023-6-65/Milena/software_install/sra_tools/sratoolkit.3.3.0-ubuntu64/bin/
+
+mkdir -p $SRA $FASTQ
+
+${SRAPATH}prefetch $1 --output-directory $SRA # Download .sra file
+${SRAPATH}fasterq-dump $SRA/$1/$1.sra -O $FASTQ --split-files --threads 8 # Convert to FASTQ (paired-end, gzipped)
+
+gunzip "${FASTQ}"/*
