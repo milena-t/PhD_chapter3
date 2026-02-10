ccbf864aef14ebb1462e0e6e1bb772c161f1c787
milena-t <milena.trabert@t-online.de>
2026-02-10 17:39:46 +0100

    number replicates

--- bash/DE_analysis/merge_lanes_Cmac_for.sh
+++ bash/DE_analysis/merge_lanes_Cmac_for.sh
@@ -9,16 +9,16 @@
 cd /proj/naiss2023-6-65/Milena/chapter3/RNAseq/maculatus/raw_data/trimmed_fastp
 
 ## forward reads
-zcat SRR3113348_1_trimmed.fastq.gz SRR3113384_1_trimmed.fastq.gz | gzip > lanes_merged_AVf_1_trimmed.fastq.gz
-zcat SRR3113350_1_trimmed.fastq.gz SRR3113385_1_trimmed.fastq.gz | gzip > lanes_merged_AVf_1_trimmed.fastq.gz
-zcat SRR3113352_1_trimmed.fastq.gz SRR3113386_1_trimmed.fastq.gz | gzip > lanes_merged_AVf_1_trimmed.fastq.gz
-zcat SRR3113420_1_trimmed.fastq.gz SRR3113365_1_trimmed.fastq.gz | gzip > lanes_merged_AVm_1_trimmed.fastq.gz
-zcat SRR3113421_1_trimmed.fastq.gz SRR3113366_1_trimmed.fastq.gz | gzip > lanes_merged_AVm_1_trimmed.fastq.gz
-zcat SRR3113422_1_trimmed.fastq.gz SRR3113367_1_trimmed.fastq.gz | gzip > lanes_merged_AVm_1_trimmed.fastq.gz
-zcat SRR3113345_1_trimmed.fastq.gz SRR3113380_1_trimmed.fastq.gz | gzip > lanes_merged_HtVf_1_trimmed.fastq.gz
-zcat SRR3113346_1_trimmed.fastq.gz SRR3113382_1_trimmed.fastq.gz | gzip > lanes_merged_HtVf_1_trimmed.fastq.gz
-zcat SRR3113347_1_trimmed.fastq.gz SRR3113383_1_trimmed.fastq.gz | gzip > lanes_merged_HtVf_1_trimmed.fastq.gz
-zcat SRR3113364_1_trimmed.fastq.gz SRR3113361_1_trimmed.fastq.gz | gzip > lanes_merged_HtVm_1_trimmed.fastq.gz
-zcat SRR3113381_1_trimmed.fastq.gz SRR3113362_1_trimmed.fastq.gz | gzip > lanes_merged_HtVm_1_trimmed.fastq.gz
-zcat SRR3113418_1_trimmed.fastq.gz SRR3113363_1_trimmed.fastq.gz | gzip > lanes_merged_HtVm_1_trimmed.fastq.gz
+zcat SRR3113348_1_trimmed.fastq.gz SRR3113384_1_trimmed.fastq.gz | gzip > lanes_merged_AVf1_1_trimmed.fastq.gz
+zcat SRR3113350_1_trimmed.fastq.gz SRR3113385_1_trimmed.fastq.gz | gzip > lanes_merged_AVf2_1_trimmed.fastq.gz
+zcat SRR3113352_1_trimmed.fastq.gz SRR3113386_1_trimmed.fastq.gz | gzip > lanes_merged_AVf3_1_trimmed.fastq.gz
+zcat SRR3113420_1_trimmed.fastq.gz SRR3113365_1_trimmed.fastq.gz | gzip > lanes_merged_AVm1_1_trimmed.fastq.gz
+zcat SRR3113421_1_trimmed.fastq.gz SRR3113366_1_trimmed.fastq.gz | gzip > lanes_merged_AVm2_1_trimmed.fastq.gz
+zcat SRR3113422_1_trimmed.fastq.gz SRR3113367_1_trimmed.fastq.gz | gzip > lanes_merged_AVm3_1_trimmed.fastq.gz
+zcat SRR3113345_1_trimmed.fastq.gz SRR3113380_1_trimmed.fastq.gz | gzip > lanes_merged_HtVf1_1_trimmed.fastq.gz
+zcat SRR3113346_1_trimmed.fastq.gz SRR3113382_1_trimmed.fastq.gz | gzip > lanes_merged_HtVf2_1_trimmed.fastq.gz
+zcat SRR3113347_1_trimmed.fastq.gz SRR3113383_1_trimmed.fastq.gz | gzip > lanes_merged_HtVf3_1_trimmed.fastq.gz
+zcat SRR3113364_1_trimmed.fastq.gz SRR3113361_1_trimmed.fastq.gz | gzip > lanes_merged_HtVm1_1_trimmed.fastq.gz
+zcat SRR3113381_1_trimmed.fastq.gz SRR3113362_1_trimmed.fastq.gz | gzip > lanes_merged_HtVm2_1_trimmed.fastq.gz
+zcat SRR3113418_1_trimmed.fastq.gz SRR3113363_1_trimmed.fastq.gz | gzip > lanes_merged_HtVm3_1_trimmed.fastq.gz
 echo ">>>>>>>> forward reads done!" 
--- bash/DE_analysis/merge_lanes_Cmac_rev.sh
+++ bash/DE_analysis/merge_lanes_Cmac_rev.sh
@@ -9,16 +9,16 @@
 cd /proj/naiss2023-6-65/Milena/chapter3/RNAseq/maculatus/raw_data/trimmed_fastp
 
 ## reverse reads
-zcat SRR3113348_2_trimmed.fastq.gz SRR3113384_2_trimmed.fastq.gz | gzip > lanes_merged_AVf_2_trimmed.fastq.gz
-zcat SRR3113350_2_trimmed.fastq.gz SRR3113385_2_trimmed.fastq.gz | gzip > lanes_merged_AVf_2_trimmed.fastq.gz
-zcat SRR3113352_2_trimmed.fastq.gz SRR3113386_2_trimmed.fastq.gz | gzip > lanes_merged_AVf_2_trimmed.fastq.gz
-zcat SRR3113420_2_trimmed.fastq.gz SRR3113365_2_trimmed.fastq.gz | gzip > lanes_merged_AVm_2_trimmed.fastq.gz
-zcat SRR3113421_2_trimmed.fastq.gz SRR3113366_2_trimmed.fastq.gz | gzip > lanes_merged_AVm_2_trimmed.fastq.gz
-zcat SRR3113422_2_trimmed.fastq.gz SRR3113367_2_trimmed.fastq.gz | gzip > lanes_merged_AVm_2_trimmed.fastq.gz
-zcat SRR3113345_2_trimmed.fastq.gz SRR3113380_2_trimmed.fastq.gz | gzip > lanes_merged_HtVf_2_trimmed.fastq.gz
-zcat SRR3113346_2_trimmed.fastq.gz SRR3113382_2_trimmed.fastq.gz | gzip > lanes_merged_HtVf_2_trimmed.fastq.gz
-zcat SRR3113347_2_trimmed.fastq.gz SRR3113383_2_trimmed.fastq.gz | gzip > lanes_merged_HtVf_2_trimmed.fastq.gz
-zcat SRR3113364_2_trimmed.fastq.gz SRR3113361_2_trimmed.fastq.gz | gzip > lanes_merged_HtVm_2_trimmed.fastq.gz
-zcat SRR3113381_2_trimmed.fastq.gz SRR3113362_2_trimmed.fastq.gz | gzip > lanes_merged_HtVm_2_trimmed.fastq.gz
-zcat SRR3113418_2_trimmed.fastq.gz SRR3113363_2_trimmed.fastq.gz | gzip > lanes_merged_HtVm_2_trimmed.fastq.gz
+zcat SRR3113348_2_trimmed.fastq.gz SRR3113384_2_trimmed.fastq.gz | gzip > lanes_merged_AVf1_2_trimmed.fastq.gz
+zcat SRR3113350_2_trimmed.fastq.gz SRR3113385_2_trimmed.fastq.gz | gzip > lanes_merged_AVf2_2_trimmed.fastq.gz
+zcat SRR3113352_2_trimmed.fastq.gz SRR3113386_2_trimmed.fastq.gz | gzip > lanes_merged_AVf3_2_trimmed.fastq.gz
+zcat SRR3113420_2_trimmed.fastq.gz SRR3113365_2_trimmed.fastq.gz | gzip > lanes_merged_AVm1_2_trimmed.fastq.gz
+zcat SRR3113421_2_trimmed.fastq.gz SRR3113366_2_trimmed.fastq.gz | gzip > lanes_merged_AVm2_2_trimmed.fastq.gz
+zcat SRR3113422_2_trimmed.fastq.gz SRR3113367_2_trimmed.fastq.gz | gzip > lanes_merged_AVm3_2_trimmed.fastq.gz
+zcat SRR3113345_2_trimmed.fastq.gz SRR3113380_2_trimmed.fastq.gz | gzip > lanes_merged_HtVf1_2_trimmed.fastq.gz
+zcat SRR3113346_2_trimmed.fastq.gz SRR3113382_2_trimmed.fastq.gz | gzip > lanes_merged_HtVf2_2_trimmed.fastq.gz
+zcat SRR3113347_2_trimmed.fastq.gz SRR3113383_2_trimmed.fastq.gz | gzip > lanes_merged_HtVf3_2_trimmed.fastq.gz
+zcat SRR3113364_2_trimmed.fastq.gz SRR3113361_2_trimmed.fastq.gz | gzip > lanes_merged_HtVm1_2_trimmed.fastq.gz
+zcat SRR3113381_2_trimmed.fastq.gz SRR3113362_2_trimmed.fastq.gz | gzip > lanes_merged_HtVm2_2_trimmed.fastq.gz
+zcat SRR3113418_2_trimmed.fastq.gz SRR3113363_2_trimmed.fastq.gz | gzip > lanes_merged_HtVm3_2_trimmed.fastq.gz
 echo ">>>>>>>> reverse reads done!" 
