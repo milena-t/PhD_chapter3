f869fd413479061739de0ee54ad269abfcbac011
milena-t <milena.trabert@t-online.de>
2026-02-23 14:35:05 +0100

    update formula

--- src/plotting/analyze_dNdS_LFC.py
+++ src/plotting/analyze_dNdS_LFC.py
@@ -5,8 +5,10 @@ use the conservation rank and A/X as fixed factors
 
 import pandas as pd
 import numpy as np
+from scipy import stats
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
+import matplotlib.pyplot as plt
 
 def get_full_table_path(username="miltr339"):
     out_dict = {
@@ -25,8 +27,8 @@ def reorder_df_to_have_dNdS_cols(full_table_paths_dict, outfile = ""):
     X_df = pd.read_csv(full_table_paths_dict["X"], sep="\t")
     A_df = pd.read_csv(full_table_paths_dict["A"], sep="\t")
     
-    X_df["chromosome"] = [1]*X_df.shape[0]
-    A_df["chromosome"] = [0]*A_df.shape[0]
+    X_df["chromosome"] = ["X"]*X_df.shape[0]
+    A_df["chromosome"] = ["A"]*A_df.shape[0]
 
     df = pd.concat([A_df,X_df], ignore_index=True)
 
@@ -51,7 +53,7 @@ def reorder_df_to_have_dNdS_cols(full_table_paths_dict, outfile = ""):
 
 
 
-def statistical_analysis(full_table_paths_dict, table_outfile=""):  
+def statistical_analysis(full_table_paths_dict, table_outfile="", max_dNdS=2):  
     """
     fit linear regression to see if the response variable (dNdS) is explained by the log2FC in either tissue, with the fixed
     factors of sex chromosome category and conservation rank. NaNs are automatically dropped
@@ -61,45 +63,90 @@ def statistical_analysis(full_table_paths_dict, table_outfile=""):
     partners_list = list(set(A_df["other_species"]))
     print(partners_list)
     df = reorder_df_to_have_dNdS_cols(full_table_paths_dict, outfile=table_outfile)
-    print(df)
-    a_df_m = df[df["LFC_abdomen"] < 0 ]
-    a_df_f = df[df["LFC_abdomen"] > 0 ]
-    ht_df_m = df[df["LFC_head+thorax"] < 0 ]
-    ht_df_f = df[df["LFC_head+thorax"] > 0 ]
-
+    df = df.rename(columns={'LFC_head+thorax': 'LFC_head_thorax'})
+    dfs_dict = { 
+        "female_biased_abdomen": df[df["LFC_abdomen"] > 0 ],
+        "male_biased_abdomen" : df[df["LFC_abdomen"] < 0  ],
+        "female_biased_head_thorax": df[df["LFC_head_thorax"] > 0 ],
+        "male_biased_head_thorax" : df[df["LFC_head_thorax"] < 0 ]
+    }
     ## test for goodness of fit between the ols with log and glm with gamma distribution models
     ## test with B_siliquastri_dN/dS
-    print(f"test goodness of fit, see code for model specification. OLS with log-transformed dNdS vsl GLM with gamma distribution")
     
     for partner in partners_list:
-        filt_df = a_df_f[a_df_f[f"{partner}_dNdS"]>0]
+        df = dfs_dict["female_biased_abdomen"]
+        filt_df = df[df[f"{partner}_dNdS"]>0]
         filt_df = filt_df[filt_df[f"{partner}_dNdS"].notna()]
+        filt_df = filt_df[filt_df[f"{partner}_dNdS"] < max_dNdS]
         print(f"* {partner}")
 
-        def test_filtering(colname):
-            print(f"   {colname} filtering:")
-            zeros_test = (a_df_f[f"{colname}"] <= 0).sum()
-            zeros_test2 = (filt_df[f"{colname}"] <= 0).sum()
-            print(f"\ttest if all 0 was removed: {zeros_test} -> {zeros_test2} (should go down to zero)")
-            nan_test = a_df_f[f"{colname}"].isna().sum()
-            nan_test2 = filt_df[f"{colname}"].isna().sum()
-            print(f"\ttest if all nan was removed: {nan_test} -> {nan_test2} (should go down to zero)")
-            infinity = np.isfinite(filt_df[f"{colname}"]).all()
-            print(f"\tall values <inf :  {infinity}")
+        if False:
+            ### test filtering stuff and see if there is anything that could make problems
+            def test_filtering(colname, nan_only=False):
+                print(f"   {colname} filtering:")
+                if nan_only:
+                    pass
+                else:
+                    zeros_test = (a_df_f[f"{colname}"] <= 0).sum()
+                    zeros_test2 = (filt_df[f"{colname}"] <= 0).sum()
+                    print(f"\ttest if all 0 was removed: {zeros_test} -> {zeros_test2} (should go down to zero)")
+                nan_test = a_df_f[f"{colname}"].isna().sum()
+                nan_test2 = filt_df[f"{colname}"].isna().sum()
+                print(f"\ttest if all nan was removed: {nan_test} -> {nan_test2} (should go down to zero)")
+                infinity = np.isfinite(filt_df[f"{colname}"]).all()
+                print(f"\tall values <inf :  {infinity}")
 
-        test_filtering(f"{partner}_dNdS")
-        test_filtering(f"LFC_abdomen")
-        test_filtering(f"chromosome")
-        test_filtering(f"level_most_dist_ortholog")
+            test_filtering(f"{partner}_dNdS")
+            test_filtering(f"LFC_abdomen")
+            test_filtering(f"chromosome", nan_only=True)
+            test_filtering(f"level_most_dist_ortholog")
 
-        ols = smf.ols("np.log(B_siliquastri_dNdS) ~ LFC_abdomen + C(chromosome) + level_most_dist_ortholog", data=filt_df).fit()
-        print(f"  - ols : {ols.aic}")
-        gamma = smf.glm(
-            "B_siliquastri_dNdS ~ LFC_abdomen + C(chromosome) + level_most_dist_ortholog",
-            data=filt_df,
-            family=sm.families.Gamma(link=sm.families.links.Log())
-        ).fit()
-        print(f"  - gamma : {gamma.aic}")
+        if True:
+            formula = f"np.log({partner}_dNdS) ~ (LFC_abdomen + LFC_head_thorax) * C(chromosome) + level_most_dist_ortholog"
+            ### ordinary least-squasres does not have normal residuals!
+            test = smf.ols(formula=formula, data=filt_df).fit(cov_type="HC3")
+            model = "OLS"
+            print(test.summary())
+            res = stats.normaltest(test.resid)
+            if False:
+                plt.scatter(x=test.fittedvalues, y=test.resid)
+                plt.axhline(0, color="red")
+                plt.ylabel("residuals")
+                plt.xlabel("fitted values")
+                plt.suptitle(f"C. maculatus and {partner} ")
+                plt.show()
+        else:
+
+            ## GLM does not work well
+            formula = f"{partner}_dNdS ~ (LFC_abdomen + LFC_head_thorax) * C(chromosome) + level_most_dist_ortholog"
+            test = smf.glm(formula=formula,data=filt_df,family=sm.families.Gamma(link=sm.families.links.Log())).fit()
+            model = "GLM"
+            print(test.summary())
+            res = stats.normaltest(test.resid_response)
+
+        if False:
+            # plot residuals distribution
+            # test normal distribution of residuals
+            if res.pvalue <0.05:
+                res_nom = f"{model} residuals NOT normally distributed"
+            else:
+                res_nom = f"{model} residuals normally distributed!"
+            if model=="OLS":
+                plt.hist(res)
+                plt.suptitle(f"C. maculatus and {partner} dNdS {model} regression residuals:\n{res_nom}")
+            else:
+                # here is the reason why GLM does not work well
+                plt.scatter(test.fittedvalues, test.resid_deviance)
+                plt.suptitle(f"C. maculatus and {partner} dNdS {model} regression\npredicted vs. observed values")
+            plt.show()
+
+        elif model == "OLS":
+            # test normal distribution of residuals
+            if res.pvalue <0.05:
+                print(f"{model} residuals NOT normally distributed")
+            else:
+                print(f"{model} residuals normally distributed!")
+
         print()
     
 
@@ -108,6 +155,6 @@ def statistical_analysis(full_table_paths_dict, table_outfile=""):
 
 if __name__ == "__main__":
 
-    username = "milena"
+    username = "miltr339"
     full_tables_dict = get_full_table_path(username=username)
     statistical_analysis(full_tables_dict, table_outfile=f"/Users/{username}/work/PhD_code/PhD_chapter3/data/DE_analysis/paml_summary_tables/paml_stats_outfile_table.tsv")
