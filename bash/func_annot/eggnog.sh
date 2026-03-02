f2631e58a63041a4ab5975c9bd004f97007d3d6c
milena-t <milena.trabert@t-online.de>
2026-03-02 14:11:23 +0100

    try again

--- bash/func_annot/eggnog.sh
+++ bash/func_annot/eggnog.sh
@@ -17,8 +17,14 @@ module load eggnog-mapper/2.1.12-foss-2024a
 INDIR=/proj/naiss2023-6-65/Milena/chapter3/Cmac_func_annot/
 InFAA=${INDIR}Cmac_superscaffolded_Lome_braker_proteins.faa
 OutDIR=${INDIR}eggnog
-
 GFF=${INDIR}Cmac_superscaffolded_Lome_braker.gtf
+
+
+## download eggnog database
+# I did:
+#   wget http://eggnog6.embl.de/download/novel_fams-1.0.1/novel_fams.dmnd.gz
+# they also suggest:
+#   python3 ${INDIR}eggnog/download_eggnog_data.py --dbname Arthropoda --data_dir ${INDIR}eggnog/eggnog_db
 
 #Use/Make scratch dir
 export scratchDIR=${SNIC_TMP}/Cmac_lome_eggnog_diamond
@@ -30,12 +36,12 @@ else
     echo "created directory in SNIC_TMP: $scratchDIR"
 fi
 
-
 #Limit annotation to Arthropods (6656)
 emapper.py \
     -i $InFAA --output_dir $OutDIR -o C_mac_eggnog_diamond \
     --cpu 16 \
     --scratch_dir $scratchDIR \
+    --data_dir ${INDIR}eggnog/eggnog_db \
     -m diamond --tax_scope 6656 \
     --decorate_gff ${GFF} \
 	--sensmode ultra-sensitive --dmnd_iterate yes --matrix BLOSUM62
