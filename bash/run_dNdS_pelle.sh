af6d980064bc448be7fdf6c2e5b5f4320e714e7b
milena-t <milena.trabert@t-online.de>
2026-05-25 15:23:08 +0200

    fix variable

--- bash/run_dNdS_pelle.sh
+++ bash/run_dNdS_pelle.sh
@@ -10,17 +10,17 @@
 ml FastTree/2.2-GCCcore-13.3.0 Biopython/1.86-gfbf-2025b PAML/4.10.9-GCCcore-13.3.0
 ## TODO check right module names
 INFILE=$1
-# bash ../../PhD_chapter3/bash/run_dNdS_pelle.sh B_siliquastri_C_maculatus_A-linked_ortholog_970.fasta
+# sbatch ../../PhD_chapter3/bash/run_dNdS_pelle.sh test_seqs_A/B_siliquastri_C_maculatus_A-linked_ortholog_970.fasta
 
 PAL2NAL_UPPMAX=/proj/naiss2023-6-65/Lila/beetle_genomes/pal2nal.v14/pal2nal.pl
 CLUSTALO_UPPMAX=/proj/naiss2023-6-65/Milena/software_install/clustal_omega/clustal-omega-1.2.4/bin/clustalo
 PAML_UPPMAX=/sw/bioinfo/paml/4.10.7/rackham/bin/codeml
 PAML_CONFIG=/sw/bioinfo/paml/4.10.7/rackham/examples/codeml.ctl
 # IN_DIR=/proj/naiss2023-6-65/Milena/chapter3/dNdS_calculations/brh_sequences_A/
-IN_DIR=/proj/naiss2023-6-65/Milena/chapter3/revision/dNdS_testing/test_seqs_A/
+# IN_DIR=/proj/naiss2023-6-65/Milena/chapter3/revision/dNdS_testing/test_seqs_A/
 
 python3 /proj/naiss2023-6-65/Milena/chapter3/PhD_chapter3/src/blast_BRH/calculate_pairwise_dNdS.py \
-    --cds "${IN_DIR}${INFILE}" \
+    --cds $INFILE \
     --pal2nalbin $PAL2NAL_UPPMAX \
     --codeml \
     --branch_pairwise \
