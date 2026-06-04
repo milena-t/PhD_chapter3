1090667d5530717b8cfa90993f84a747a7b97e00
milena-t <milena.trabert@t-online.de>
2026-06-04 17:14:26 +0200

    update pos sel bruchini test

--- src/blast_BRH/multiple_testing_correction_site_model_LRT.py
+++ src/blast_BRH/multiple_testing_correction_site_model_LRT.py
@@ -12,6 +12,8 @@ def get_log_file_paths(username="miltr339"):
     out_dict = {
         "A" : f"/Users/{username}/work/chapter3/dNdS/site_model_A_all_log_files.out",
         "X" : f"/Users/{username}/work/chapter3/dNdS/site_model_X_all_log_files.out",
+        "A_rev" : f"/Users/{username}/work/pairwise_blast_chapter_2_3/brh_tables/bruchini_seqs_revision_A/run.log",
+        "X_rev" : f"/Users/{username}/work/pairwise_blast_chapter_2_3/brh_tables/bruchini_seqs_revision_X/run.log",
     }
     return out_dict
 
@@ -134,14 +136,29 @@ if __name__ == "__main__":
         "X_LRT" : f"/Users/{username}/work/pairwise_blast_chapter_2_3/brh_tables/brh_results_X/site_classes_summary_X-linked.txt",
         "A_LRT_BH_corr" : f"/Users/{username}/work/PhD_code/PhD_chapter3/data/DE_analysis/paml_summary_tables/site_classes_summary_A_BH_corrected.txt",
         "X_LRT_BH_corr" : f"/Users/{username}/work/PhD_code/PhD_chapter3/data/DE_analysis/paml_summary_tables/site_classes_summary_X_BH_corrected.txt",
+        "A_rev_LRT" : f"/Users/{username}/work/pairwise_blast_chapter_2_3/brh_tables/brh_results_A/site_classes_summary_A-linked.txt",
+        "X_rev_LRT" : f"/Users/{username}/work/pairwise_blast_chapter_2_3/brh_tables/brh_results_X/site_classes_summary_X-linked.txt",
+        "A_rev_LRT_BH_corr" : f"/Users/{username}/work/PhD_code/PhD_chapter3/data/DE_analysis/paml_summary_tables/bruchini_rev_site_classes_summary_A_BH_corrected.txt",
+        "X_rev_LRT_BH_corr" : f"/Users/{username}/work/PhD_code/PhD_chapter3/data/DE_analysis/paml_summary_tables/bruchini_rev_site_classes_summary_X_BH_corrected.txt",
+        "A_list_rev" : f"/Users/{username}/work/PhD_code/PhD_chapter3/data/DE_analysis/paml_summary_tables/bruchini_rev_site_classes_list_A_BH_corrected.txt",
+        "X_list_rev" : f"/Users/{username}/work/PhD_code/PhD_chapter3/data/DE_analysis/paml_summary_tables/bruchini_rev_site_classes_list_X_BH_corrected.txt",
     }
 
     chromosomes = ["X", "A"]
     # chromosomes = ["X"]
-    
-    for chromosome in chromosomes:
-        print(f"////////////////////////// {chromosome} //////////////////////////")
-        pvals_dict = get_pvals_dict_from_log(log_file_path=log_paths_dict[chromosome])
-        bh_pvals_dict = multiple_testing_correction(pvals_dict)
-        pos_sel_orthologs = write_output(bh_pvals_dict, p_sig=p_sig, outfile_path=files[f"{chromosome}_list"])
-        modify_original_file(site_classes_path=files[f"{chromosome}_LRT"], bh_pos_sel_orthologs=pos_sel_orthologs, outfile=files[f"{chromosome}_LRT_BH_corr"])
+    if False:
+        for chromosome in chromosomes:
+            print(f"////////////////////////// {chromosome} //////////////////////////")
+            pvals_dict = get_pvals_dict_from_log(log_file_path=log_paths_dict[chromosome])
+            bh_pvals_dict = multiple_testing_correction(pvals_dict)
+            pos_sel_orthologs = write_output(bh_pvals_dict, p_sig=p_sig, outfile_path=files[f"{chromosome}_list"])
+            modify_original_file(site_classes_path=files[f"{chromosome}_LRT"], bh_pos_sel_orthologs=pos_sel_orthologs, outfile=files[f"{chromosome}_LRT_BH_corr"])
+
+    ## revisions
+    if True:
+        for chromosome in chromosomes:
+            print(f"////////////////////////// {chromosome} //////////////////////////")
+            pvals_dict = get_pvals_dict_from_log(log_file_path=log_paths_dict[f"{chromosome}_rev"])
+            bh_pvals_dict = multiple_testing_correction(pvals_dict)
+            pos_sel_orthologs = write_output(bh_pvals_dict, p_sig=p_sig, outfile_path=files[f"{chromosome}_list_rev"])
+            modify_original_file(site_classes_path=files[f"{chromosome}_rev_LRT"], bh_pos_sel_orthologs=pos_sel_orthologs, outfile=files[f"{chromosome}_rev_LRT_BH_corr"])
