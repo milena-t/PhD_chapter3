ab3e526956b24feb005a413b1797577380cefc50
milena-t <milena.trabert@t-online.de>
2026-06-23 23:16:44 +0200

    try last alignment

---
+++ bash/MK_test/align_outgroup_npstat.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -A uppmax2026-1-8
+#SBATCH -c 8
+#SBATCH --mem=72G
+#SBATCH -t 3-00:00:00
+#SBATCH -J align_outgroup
+#SBATCH -o align_outgroup.log
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user milena.trabert@ebc.uu.se
+
+
+LAST_PATH=/gorilla/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/software_install/last-aligner/last-1651/bin/
+SPECIES=B_siliquastri
+
+# make reference database and train parameters on query species
+${LAST_PATH}lastdb -P8 Cmacdb assemblies/C_maculatus.fasta.masked
+${LAST_PATH}last-train -P8 --revsym -C2 Cmacdb assemblies/${SPECIES}.fasta.masked > C_maculatus_${SPECIES}.train
+# do alignment
+${LAST_PATH}lastal -P8 -m2 -D1e9 -C2 --split -p hl.train humdb lemur.fa | ${LAST_PATH}last-split -r | ${LAST_PATH}maf-linked - > out.maf
+
+${LAST_PATH}last-dotplot out.maf
