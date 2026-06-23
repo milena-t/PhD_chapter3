b502fc89556d829a255f8b3157c2813b6c211c2c
milena-t <milena.trabert@t-online.de>
2026-06-23 17:22:59 +0200

    liftover bam

---
+++ bash/MK_test/coordinate_convert_pileup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -A uppmax2026-1-8
+#SBATCH -c 1
+#SBATCH --mem=72G
+#SBATCH -t 3:00:00
+#SBATCH -J liftover_bam
+#SBATCH -o liftover_bam.log
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user milena.trabert@ebc.uu.se
+
+
+cd /gorilla/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter3/revision/MK_test/npstat_MK_test
+
+CrossMap bam utg_to_superscaffold.chain UF-2981-ANC_S1_L001.bam assemblies/C_maculatus.fasta.masked UF-2981-ANC_S1_L001.superscaffolded.bam
