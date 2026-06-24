d376810ce44eac355cd4a6e0b5d8ca691fbf106d
milena-t <milena.trabert@t-online.de>
2026-06-24 11:29:12 +0200

    run cchi

--- bash/MK_test/align_outgroup_npstat.sh
+++ bash/MK_test/align_outgroup_npstat.sh
@@ -16,11 +16,13 @@ SPECIES=$1
 SPECIES=C_chinensis
 cd $ASS_DIR
 
-# make reference database and train parameters on query species
-# ${LAST_PATH}lastdb -P8 Cmacdb C_maculatus.fasta.masked
 if [ 1 == 1 ] ; then
+    
+    # make reference database and train parameters on query species
+    # ${LAST_PATH}lastdb -P8 Cmacdb C_maculatus.fasta.masked
+    
     echo "running ------- last-train "
-    ${LAST_PATH}last-train -P8 --revsym -C2 Cmacdb ${SPECIES}.fasta.masked > C_maculatus_${SPECIES}.train
+    # ${LAST_PATH}last-train -P8 --revsym -C2 Cmacdb ${SPECIES}.fasta.masked > C_maculatus_${SPECIES}.train
     echo "done ---------- last-train "
 
     # do alignment
