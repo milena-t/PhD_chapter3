a12219b5d758e0901ea9b387527b9d8123472819
milena-t <milena.trabert@t-online.de>
2026-06-24 13:01:49 +0200

    todo check output

--- bash/MK_test/align_outgroup_npstat.sh
+++ bash/MK_test/align_outgroup_npstat.sh
@@ -13,10 +13,10 @@ ml GCCcore/14.3.0 BEDTools/2.31.1-GCC-13.3.0
 LAST_PATH=/gorilla/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/software_install/last-aligner/last-1651/bin/
 ASS_DIR=/gorilla/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter3/revision/MK_test/npstat_MK_test/assemblies
 SPECIES=$1
-SPECIES=C_chinensis
+SPECIES=B_siliquastri
 cd $ASS_DIR
 
-if [ 1 == 1 ] ; then
+if [ 1 == 2 ] ; then
     
     # make reference database and train parameters on query species
     # ${LAST_PATH}lastdb -P8 Cmacdb C_maculatus.fasta.masked
@@ -27,7 +27,7 @@ if [ 1 == 1 ] ; then
 
     # do alignment
     echo "running ------- lastal "
-    ${LAST_PATH}lastal -P8 -m2 -D1e9 -C2 --split -p C_maculatus_${SPECIES}.train Cmacdb ${SPECIES}.fasta.masked | ${LAST_PATH}last-split -r | ${LAST_PATH}maf-linked - > out_${SPECIES}.maf
+    ${LAST_PATH}lastal -P8 -f MAF -m2 -D1e9 -C2 --split -p C_maculatus_${SPECIES}.train Cmacdb ${SPECIES}.fasta.masked | ${LAST_PATH}last-split -r | ${LAST_PATH}maf-linked - > out_${SPECIES}.maf
     echo "done ---------- lastal "
 
     echo "running ------- last-dotplot "
@@ -35,8 +35,26 @@ if [ 1 == 1 ] ; then
     echo "done ---------- last-dotplot "
 fi
 
-echo "running ------- check coverage"
-${LAST_PATH}maf-convert bed out_${SPECIES}.maf > aln_${SPECIES}.bed
-sort -k1,1 -k2,2n aln_${SPECIES}.bed > aln_${SPECIES}.sorted.bed
-bedtools merge -i aln_${SPECIES}.sorted.bed > aln_${SPECIES}.merged.bed
-awk '{sum += $3-$2} END {print sum}' aln_${SPECIES}.merged.bed
+if [ 1 == 2] ; then
+
+    echo "running ------- check coverage"
+    ${LAST_PATH}maf-convert bed out_${SPECIES}.maf | sort -k1,1 -k2,2n > aln_${SPECIES}.sorted.bed
+    bedtools merge -i aln_${SPECIES}.sorted.bed > aln_${SPECIES}.merged.bed
+    awk '{sum += $3-$2} END {print sum}' aln_${SPECIES}.merged.bed
+
+fi
+
+if [ 1 == 2] ; then
+    MAFTOOLS_PATH=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/software_install/mafTools/mafTools/bin/
+    ${MAFTOOLS_PATH}mafToFastaStitcher \
+        --maf out_${SPECIES}.maf \
+        --seqs C_maculatus.fasta.masked,${SPECIES}.fasta.masked \
+        --breakpointPenalty 1 \
+        --interstitialSequence 0 \
+        --outMfa projected_aln_C_maculatus_${SPECIES}.mfa
+
+fi
+
+head -3 out_${SPECIES}.maf
+wc -l out_${SPECIES}.maf
+ls -la out_${SPECIES}.maf
