771a93a1290944587c69d1c026ca30e75c5354c6
milena-t <milena.trabert@t-online.de>
2026-06-25 13:20:51 +0200

    batch rev

--- src/blast_BRH/calculate_batch_pw_dNdS.py
+++ src/blast_BRH/calculate_batch_pw_dNdS.py
@@ -53,6 +53,8 @@ def chunks(lst, n):
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
 
+
+
 if __name__ == "__main__":
 
     # list files in dir
@@ -62,7 +64,11 @@ if __name__ == "__main__":
     pelle = False
 
     if not os.path.isdir(datadir):
-        datadir = f"/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter3/dNdS_calculations/"
+        # original pairwise
+        # datadir = f"/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter3/dNdS_calculations/"
+        # revisions
+        datadir = f"/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter3/revision/site_model_bruchini/"
+
         bash_dir = f"/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter3/PhD_chapter3/bash"
         dNdS_exec = f"sbatch"
         pelle = True
@@ -79,24 +85,40 @@ if __name__ == "__main__":
     #######
     
     # analysis = "LRT" ## codeml site model M1a and M2a with likelihood ratio test
-    analysis = "dNdS" ## codeml branch model
+    # analysis = "dNdS" ## codeml branch model
+    
+    #### revision
+    analysis = "M7-M8_LRT"
+    # analysis = "LRT"
+    # analysis = "dNdS"
     
     #######
     ###############
     #############################
-    ## done: A_dNdS, X_dNdS, X_LRT, A_LRT
     ## revision: 
 
-    script_path = f"{bash_dir}/run_batch_{analysis}_{chr_type}.sh"
+    script_path = f"{bash_dir}/run_batch_{analysis}_bruchini_revision.sh"
+    if analysis == "M7-M8_LRT":
+        chr_type = "A"
+        outdir = f"{datadir}branch_model_res_{chr_type}/"
+    elif analysis == "LRT":
+        chr_type = "A"
+        outdir = f"{datadir}site_model_res_{chr_type}/"
+    elif analysis == "dNdS":
+        chr_type = "A"
+        outdir = f"{datadir}site_model_res_{chr_type}/"
+    X_path = f"{datadir}bruchini_fasta_{chr_type}/"
 
-    ### original
-    X_path = f"{datadir}brh_sequences_{chr_type}/"
-    if analysis=="dNdS":
-        outdir = f"{datadir}brh_results_{chr_type}_branch_model/"
-    elif analysis=="LRT":
-        outdir = f"{datadir}brh_results_{chr_type}/"
-    else:
-        raise RuntimeError(f"invalid 'analysis' option! you have {analysis} but it needs to be 'dNdS' or 'LRT' ")
+    ### original pairwise analysis
+    ## done: A_dNdS, X_dNdS, X_LRT, A_LRT
+    if False:
+        X_path = f"{datadir}brh_sequences_{chr_type}/"
+        if analysis=="dNdS":
+            outdir = f"{datadir}brh_results_{chr_type}_branch_model/"
+        elif analysis=="LRT":
+            outdir = f"{datadir}brh_results_{chr_type}/"
+        else:
+            raise RuntimeError(f"invalid 'analysis' option! you have {analysis} but it needs to be 'dNdS' or 'LRT' ")
 
     ### second run for the ancestral X-syntenic chromosome in Dcar as X
     # X_path = f"{datadir}brh_sequences_{chr_type}_Dcar_X_syntenic/"
@@ -105,74 +127,111 @@ if __name__ == "__main__":
     os.chdir(outdir)
     # x_paths_nested_dict = make_nested_lists(X_path, include_list=["C_magnifica","C_septempunctata","T_castaneum","T_freemani"])
     # x_paths_nested_dict = make_nested_lists(X_path, include_list=["A_obtectus","B_siliquastri","C_chinensis","C_maculatus"])
-    x_paths_nested_dict = make_nested_lists(X_path) # include all species if not specified
     
-    sp1_list=list(x_paths_nested_dict.keys())
+    
+    ########## REVISIONS 
+    
+    if True:
+        x_paths_nested_dict = make_nested_lists(X_path) # include all species if not specified
+    
+        sp1_list=list(x_paths_nested_dict.keys())
 
-    ########
-    ## for testing purposes only do a few files of each pair
-    ## if 0 then it takes all files, for a normal run
-    num_files = 0
-    ########
+        ########
+        ## for testing purposes only do a few files of each pair
+        ## if 0 then it takes all files, for a normal run
+        num_files = 0
+        ########
 
-    for species1, subdict in x_paths_nested_dict.items():
-        print(f"species1 : {species1}")
-        # if species1 == "C_magnifica"  or species1 == "C_septempunctata":
-        #     print(f"run {species1}")
-        #     pass
-        # elif species1 == "T_castaneum"  or species1 == "T_freemani":
-        #     print(f"run {species1}")
-        #     pass
-        # else:
-        #     print(f"skip {species1}")
-        #     continue
+                
+        if chr_type == "A" and num_files == 0:
+            ### split into several batches because it doesn't run with too many fasta files
+            fasta_overall_list = [f"{datadir}{f}" for f in os.listdir(datadir) if os.path.isfile(os.path.join(datadir, f))]
+            i = 0
+            for fasta_list in chunks(fasta_overall_list, 500):
+                fasta_string = " ".join([f"{fasta}" for fasta in fasta_list])
 
-        for species2, fasta_list in subdict.items():
-            if species1 == species2:
-                continue
-            dirname = f"{species1}_{species2}_pairwise_dNdS"
-            print(f">>>> {dirname}\n")
-            if not os.path.isdir(dirname):
-                os.mkdir(dirname)
-            os.chdir(dirname)
-            wd = os.getcwd()
-
-            if chr_type == "X" or num_files!=0:
-                if num_files == 0:
-                    fasta_string = " ".join([f"{fasta}" for fasta in fasta_list])
-                else:
-                    fasta_string = " ".join([f"{fasta}" for fasta in fasta_list[:num_files]])
-                print(f"\t--> working in {wd}")
                 
                 if pelle:
-                    jobname = f"dNdS_{chr_type}-linked_{species1}_{species2}"
+                    jobname = f"{i}_{analysis}_{chr_type}-linked"
                     dNdS_command = f"{dNdS_exec} -J {jobname} -o {jobname}.out {script_path} {fasta_string}"
                 else:
                     dNdS_command = f"{dNdS_exec} {script_path} {fasta_string}"
 
                 os.system(dNdS_command)
+                os.system("sleep .5") # wait a little bit after each command so that the job manager can keep up
                 print(f"{dNdS_command[:1000]} ...")
-            
-            if chr_type == "A" and num_files == 0:
-                ### split into several batches because it doesn't run with too many fasta files
-                fasta_overall_list = fasta_list
-                i = 0
-                for fasta_list in chunks(fasta_overall_list, 500):
-                    fasta_string = " ".join([f"{fasta}" for fasta in fasta_list])
+                i +=1
+
+    else:
+
+        x_paths_nested_dict = make_nested_lists(X_path) # include all species if not specified
+        
+        sp1_list=list(x_paths_nested_dict.keys())
+
+        ########
+        ## for testing purposes only do a few files of each pair
+        ## if 0 then it takes all files, for a normal run
+        num_files = 0
+        ########
+
+        for species1, subdict in x_paths_nested_dict.items():
+            print(f"species1 : {species1}")
+            # if species1 == "C_magnifica"  or species1 == "C_septempunctata":
+            #     print(f"run {species1}")
+            #     pass
+            # elif species1 == "T_castaneum"  or species1 == "T_freemani":
+            #     print(f"run {species1}")
+            #     pass
+            # else:
+            #     print(f"skip {species1}")
+            #     continue
+
+            for species2, fasta_list in subdict.items():
+                if species1 == species2:
+                    continue
+                dirname = f"{species1}_{species2}_pairwise_dNdS"
+                print(f">>>> {dirname}\n")
+                if not os.path.isdir(dirname):
+                    os.mkdir(dirname)
+                os.chdir(dirname)
+                wd = os.getcwd()
+
+                if chr_type == "X" or num_files!=0:
+                    if num_files == 0:
+                        fasta_string = " ".join([f"{fasta}" for fasta in fasta_list])
+                    else:
+                        fasta_string = " ".join([f"{fasta}" for fasta in fasta_list[:num_files]])
                     print(f"\t--> working in {wd}")
                     
                     if pelle:
-                        jobname = f"{i}_dNdS_{chr_type}-linked_{species1}_{species2}"
+                        jobname = f"dNdS_{chr_type}-linked_{species1}_{species2}"
                         dNdS_command = f"{dNdS_exec} -J {jobname} -o {jobname}.out {script_path} {fasta_string}"
                     else:
                         dNdS_command = f"{dNdS_exec} {script_path} {fasta_string}"
 
                     os.system(dNdS_command)
-                    os.system("sleep .5") # wait a little bit after each command so that the job manager can keep up
                     print(f"{dNdS_command[:1000]} ...")
-                    i +=1
+                
+                if chr_type == "A" and num_files == 0:
+                    ### split into several batches because it doesn't run with too many fasta files
+                    fasta_overall_list = fasta_list
+                    i = 0
+                    for fasta_list in chunks(fasta_overall_list, 500):
+                        fasta_string = " ".join([f"{fasta}" for fasta in fasta_list])
+                        print(f"\t--> working in {wd}")
+                        
+                        if pelle:
+                            jobname = f"{i}_dNdS_{chr_type}-linked_{species1}_{species2}"
+                            dNdS_command = f"{dNdS_exec} -J {jobname} -o {jobname}.out {script_path} {fasta_string}"
+                        else:
+                            dNdS_command = f"{dNdS_exec} {script_path} {fasta_string}"
 
-            os.chdir("..")
-            wd = os.getcwd()
-            # print(f"\n working in {wd}")
-            
+                        os.system(dNdS_command)
+                        os.system("sleep .5") # wait a little bit after each command so that the job manager can keep up
+                        print(f"{dNdS_command[:1000]} ...")
+                        i +=1
+
+                os.chdir("..")
+                wd = os.getcwd()
+                # print(f"\n working in {wd}")
+                
